7c136b75b4c5cf1dbf8f379cf9b8749d9384ebe4
haridwariyaas-ai <haridwariya.as@gmail.com>
2026-03-12 23:27:25 +0530

    Format code for improved readability

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
 import streamlit as st
-
 import ai_engine
 
 st.title("DEVI AI Trading Brain")
 
-model,data = ai_engine.train_model()
+model, data = ai_engine.train_model()
 
-signal,bull,bear,price = ai_engine.predict_signal(model,data)
+signal, bull, bear, price = ai_engine.predict_signal(model, data)
 
-st.metric("NIFTY Price", round(price,2))
-
+st.metric("NIFTY Price", round(price, 2))
 st.metric("AI Signal", signal)
-
 st.metric("Bullish %", bull)
-
 st.metric("Bearish %", bear)
