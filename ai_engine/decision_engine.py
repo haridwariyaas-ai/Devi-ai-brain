8b943d93cd419a23b06ca5ae6acf006013e627d9
haridwariyaas-ai <haridwariya.as@gmail.com>
2026-03-13 21:48:55 +0530

    Refactor decision engine by removing make_decision
    Removed the make_decision function and added market_bias function.

--- ai_engine/decision_engine.py
+++ ai_engine/decision_engine.py
@@ -1,13 +1,3 @@
+def market_bias():
 
-def make_decision(option_chain):
-
-    pcr = option_chain.get("PCR",1)
-
-    if pcr > 1.1:
-        return "BULLISH"
-
-    elif pcr < 0.9:
-        return "BEARISH"
-
-    else:
-        return "SIDEWAYS"
+    return "Sideways"
