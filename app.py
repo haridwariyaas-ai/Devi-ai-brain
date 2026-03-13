dd8bdd9a2ac2e1f65aa50a80678a2a4c8b317acb
haridwariyaas-ai <haridwariya.as@gmail.com>
2026-03-13 23:19:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-
 import streamlit as st
 from ai_engine.brain import DeviBrain
 
-st.title("Devi AI Brain")
+st.title("Devi AI Brain V4")
 
 brain = DeviBrain()
 
 if st.button("Run AI Analysis"):
+
     result = brain.run_cycle()
+
     st.json(result)
