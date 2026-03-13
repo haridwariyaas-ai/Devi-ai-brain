6b90f9714f933ab92393009b192cef721fc41212
haridwariyaas-ai <haridwariya.as@gmail.com>
2026-03-13 21:47:25 +0530

    Refactor DeviBrain to use upstox_data and simplify logic

--- ai_engine/brain.py
+++ ai_engine/brain.py
@@ -1,40 +1,26 @@
-from market.nifty_data import get_nifty_price
-from market.option_chain import get_option_chain
+from market_data.upstox_data import get_nifty_price
 from utils.atm import find_atm
 from ai_engine.decision import market_bias
 from ai_engine.strategy import build_strategy
-from utils.risk import position_size
-from ai_engine.learning import LearningBrain
 
 
 class DeviBrain:
-
-    def __init__(self):
-
-        self.memory = LearningBrain()
 
     def run_cycle(self):
 
         price = get_nifty_price()
 
-        option_chain = get_option_chain()
-
         atm = find_atm(price)
 
-        bias = market_bias(option_chain["PCR"])
+        bias = market_bias()
 
-        strategy = build_strategy(bias, atm)
+        strategy = build_strategy(atm)
 
-        size = position_size(100000, 2)
+        return {
 
-        trade = {
-            "price": price,
-            "atm": atm,
-            "bias": bias,
-            "strategy": strategy,
-            "size": size
+            "NIFTY_PRICE": price,
+            "ATM_STRIKE": atm,
+            "MARKET_BIAS": bias,
+            "TRADE_IDEA": strategy
+
         }
-
-        self.memory.store(trade)
-
-        return trade
