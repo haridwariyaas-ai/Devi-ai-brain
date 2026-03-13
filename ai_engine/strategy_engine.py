92a1eabb0d8bf6e9a9e3652f2b21ee9f8b7a8315
haridwariyaas-ai <haridwariya.as@gmail.com>
2026-03-13 21:55:54 +0530

    Refactor strategy functions in strategy_engine.py
    Removed the build_trade function and added a new build_strategy function that returns a sell strangle message.

--- ai_engine/strategy_engine.py
+++ ai_engine/strategy_engine.py
@@ -1,20 +1,3 @@
+def build_strategy(atm):
 
-def build_trade(direction, atm):
-
-    if direction == "BULLISH":
-        return {
-            "strategy": "SELL_PE",
-            "strike": atm
-        }
-
-    elif direction == "BEARISH":
-        return {
-            "strategy": "SELL_CE",
-            "strike": atm
-        }
-
-    else:
-        return {
-            "strategy": "IRON_CONDOR",
-            "center": atm
-        }
+    return f"Sell Strangle around {atm}"
