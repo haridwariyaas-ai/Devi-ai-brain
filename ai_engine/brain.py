7cdd81ad7446dcccb8c50f0ae92777b987055fd4
haridwariyaas-ai <haridwariya.as@gmail.com>
2026-03-14 23:30:56 +0530

    Update brain.py

--- ai_engine/brain.py
+++ ai_engine/brain.py
@@ -2,7 +2,9 @@ from market_data.price_data import get_nifty_price
 from utils.atm import find_atm
 from analysis.oi_analysis import detect_bias
 from ai_engine.strategy_generator import generate_strategy
-
+from analysis.support_resistance import calculate_support_resistance
+from analysis.candlestick_ai import detect_candle
+from analysis.probability_engine import calculate_probability
 class DeviBrain:
 
     def run_cycle(self):
