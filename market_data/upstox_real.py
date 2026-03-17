eaaee3be16028900244189efb323441d5c8a46f0
haridwariyaas-ai <haridwariya.as@gmail.com>
2026-03-17 21:31:07 +0530

    Create upstox_real.py

---
+++ market_data/upstox_real.py
@@ -0,0 +1,34 @@
+import requests
+import os
+
+def get_upstox_price():
+
+    token = os.getenv("UPSTOX_ACCESS_TOKEN")
+
+    if not token:
+        print("Token missing")
+        return None
+
+    url = "https://api.upstox.com/v2/market-quote/ltp"
+
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Accept": "application/json"
+    }
+
+    params = {
+        "instrument_key": "NSE_INDEX|Nifty 50"
+    }
+
+    try:
+        r = requests.get(url, headers=headers, params=params)
+
+        data = r.json()
+
+        if "data" in data and "NSE_INDEX|Nifty 50" in data["data"]:
+            return data["data"]["NSE_INDEX|Nifty 50"]["last_price"]
+
+    except Exception as e:
+        print("Upstox Error:", e)
+
+    return None
