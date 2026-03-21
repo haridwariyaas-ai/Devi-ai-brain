593609b333e2759c460b2ccc7b07eafb71637a01
haridwariyaas-ai <haridwariya.as@gmail.com>
2026-03-21 22:04:13 +0530

    Update brain.py

--- ai_engine/brain.py
+++ ai_engine/brain.py
@@ -1,32 +1,25 @@
-print("🔥🔥 NEW BRAIN FILE RUNNING 🔥🔥")
-
 from market_data.upstox_real import get_upstox_price
-from market_data.price_data import get_nifty_price
-from market_data.nse_oi import get_nse_oi
+from market_data.upstox_oi import get_upstox_oi
 
 
 class DeviBrain:
 
     def run_cycle(self):
 
-        print("🔥 STEP 1 — PRICE FETCH")
+        print("🔥 FETCH PRICE")
 
         price = get_upstox_price()
 
-        if price is None:
-            print("⚠️ fallback price used")
-            price = get_nifty_price()
+        print("🔥 PRICE:", price)
 
-        print("🔥 FINAL PRICE:", price)
+        print("🔥 FETCH OI")
 
-        print("🔥 STEP 2 — CALLING NSE OI")
-
-        oi_data = get_nse_oi(price)
+        oi_data = get_upstox_oi(price)
 
         call_oi = oi_data["call_oi"]
         put_oi = oi_data["put_oi"]
 
-        print("🔥 FINAL OI:", call_oi, put_oi)
+        print("🔥 OI:", call_oi, put_oi)
 
         return {
             "NIFTY_PRICE": price,
