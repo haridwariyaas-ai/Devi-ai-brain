0a63f2f422bf888a5047690094f54b65e3e8b62a
haridwariyaas-ai <haridwariya.as@gmail.com>
2026-03-23 21:42:00 +0530

    Update brain.py

--- ai_engine/brain.py
+++ ai_engine/brain.py
@@ -1,32 +1,15 @@
-from market_data.upstox_price import get_nifty_ltp
-from market_data.upstox_oi import get_option_oi
-from market_data.instruments import (
-    load_instruments,
-    get_nearest_expiry_df,
-    get_atm_options
-)
+from market_data.upstox_price import get_price
+from market_data.upstox_oi import get_oi
 
 
 class DeviBrain:
 
     def run_cycle(self):
 
-        nifty_df = load_instruments()
-        nifty_exp_df = get_nearest_expiry_df(nifty_df)
-
-        price = get_nifty_ltp()
-
-        if price == 0:
-            return {"error": "Price not available"}
-
-        ce_key, pe_key, strike = get_atm_options(nifty_exp_df, price)
-
-        oi = get_option_oi(ce_key, pe_key)
+        price = get_price()
+        oi = get_oi(price)
 
         return {
             "price": price,
-            "strike": strike,
-            "oi": oi,
-            "ce_key": ce_key,
-            "pe_key": pe_key
+            "oi": oi
         }
