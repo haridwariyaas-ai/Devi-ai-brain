6340bba5754cf219801fee230b6744cdcbdee4df
haridwariyaas-ai <haridwariya.as@gmail.com>
2026-03-23 21:42:41 +0530

    Update upstox_price.py

--- market_data/upstox_price.py
+++ market_data/upstox_price.py
@@ -1,8 +1,13 @@
 import requests
 import os
 
-def get_nifty_ltp():
+
+def get_price():
+
     token = os.getenv("UPSTOX_ACCESS_TOKEN")
+
+    if not token:
+        return 0
 
     url = "https://api.upstox.com/v2/market-quote/ltp"
 
@@ -14,10 +19,14 @@ def get_nifty_ltp():
         "instrument_key": "NSE_INDEX|Nifty 50"
     }
 
-    r = requests.get(url, headers=headers, params=params).json()
+    try:
+        r = requests.get(url, headers=headers, params=params, timeout=5)
+        data = r.json()
 
-    if "data" not in r:
-        print("PRICE ERROR:", r)
+        if "data" not in data:
+            return 0
+
+        return list(data["data"].values())[0]["last_price"]
+
+    except:
         return 0
-
-    return list(r["data"].values())[0]["last_price"]
