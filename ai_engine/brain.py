9d363f939c089d067966ec72edf3341046561fb1
haridwariyaas-ai <haridwariya.as@gmail.com>
2026-03-23 22:13:08 +0530

    Update brain.py

--- ai_engine/brain.py
+++ ai_engine/brain.py
@@ -1,15 +1,16 @@
-from market_data.upstox_price import get_price
-from market_data.upstox_oi import get_oi
-
+from market_data.upstox_real import get_upstox_price
+from market_data.upstox_oi import get_upstox_oi
 
 class DeviBrain:
 
     def run_cycle(self):
 
-        price = get_price()
-        oi = get_oi(price)
+        price = get_upstox_price()
+
+        oi = get_upstox_oi(price)
 
         return {
-            "price": price,
-            "oi": oi
+            "NIFTY_PRICE": price,
+            "CALL_OI": oi["call_oi"],
+            "PUT_OI": oi["put_oi"]
         }
