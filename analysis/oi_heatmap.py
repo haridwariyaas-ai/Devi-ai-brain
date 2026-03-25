748c4c0e3dc85dd59a36d610d131705009b051b6
haridwariyaas-ai <haridwariya.as@gmail.com>
2026-03-25 22:34:09 +0530

    Delete analysis/oi_heatmap.py

--- analysis/oi_heatmap.py
+++
@@ -1,9 +0,0 @@
-def oi_heatmap(option_chain):
-
-    max_call = max(option_chain, key=lambda x: x["call_oi"])
-    max_put = max(option_chain, key=lambda x: x["put_oi"])
-
-    resistance = max_call["strike"]
-    support = max_put["strike"]
-
-    return support, resistance
