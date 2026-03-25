c55045b41ef3960dba3d4bd6cc7dc0dd8715d9f0
haridwariyaas-ai <haridwariya.as@gmail.com>
2026-03-25 22:36:05 +0530

    Delete analysis/risk_manager.py

--- analysis/risk_manager.py
+++
@@ -1,9 +0,0 @@
-def risk_manager(probability, trend):
-
-    if probability >= 70 and trend == "Uptrend":
-        return "Low Risk"
-
-    if probability >= 60:
-        return "Medium Risk"
-
-    return "High Risk"
