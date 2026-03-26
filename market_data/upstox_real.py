85aa0e649942372aa1ed3aa73ee5d50a1347c2bc
haridwariyaas-ai <haridwariya.as@gmail.com>
2026-03-26 21:46:20 +0530

    Update upstox_real.py

--- market_data/upstox_real.py
+++ market_data/upstox_real.py
@@ -1,33 +1,25 @@
 import requests
 import os
 
-print("🔥 upstox_real loaded")
-
 def get_nifty_price():
     try:
         token = os.getenv("UPSTOX_ACCESS_TOKEN")
 
-        if not token:
-            return 0
-
-        url = "https://api.upstox.com/v2/market-quote/ltp"
-
         headers = {
+            "Accept": "application/json",
             "Authorization": f"Bearer {token}"
         }
 
-        params = {
-            "instrument_key": "NSE_INDEX|Nifty 50"
-        }
+        url = "https://api.upstox.com/v2/market-quote/ltp"
+        params = {"instrument_key": "NSE_INDEX|Nifty 50"}
 
-        res = requests.get(url, headers=headers, params=params)
-        data = res.json()
+        r = requests.get(url, headers=headers, params=params).json()
 
-        if data.get("status") != "success":
+        if r.get("status") != "success":
             return 0
 
-        key = list(data["data"].keys())[0]
-        return data["data"][key]["last_price"]
+        key = list(r["data"].keys())[0]
+        return r["data"][key]["last_price"]
 
     except:
         return 0
