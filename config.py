ae2f7ff82f4bcee102fdb1b34394878667194b79
haridwariyaas-ai <haridwariya.as@gmail.com>
2026-04-19 20:05:37 +0530

    Refactor get_access_token function for simplicity

--- config.py
+++ config.py
@@ -2,12 +2,6 @@
 
 import streamlit as st
 
-def get_access_token():
-    # Session se token lo (login ke baad)
-    if "access_token" in st.session_state:
-        return st.session_state["access_token"]
-    return None
-
 def get_api_details():
     try:
         api_key = st.secrets["UPSTOX_API_KEY"]
@@ -17,5 +11,8 @@ def get_api_details():
     except:
         return None, None, None
 
-ACCESS_TOKEN = get_access_token()
+def get_access_token():
+    return st.session_state.get("access_token", None)
+
 API_KEY, API_SECRET, REDIRECT_URI = get_api_details()
+ACCESS_TOKEN = get_access_token()
