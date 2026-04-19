e15a26567a3aa4c830d9ed5f8e38c8a4c8f6825f
haridwariyaas-ai <haridwariya.as@gmail.com>
2026-04-19 20:18:42 +0530

    Refactor get_api_details and get_access_token functions

--- config.py
+++ config.py
@@ -1,18 +1,14 @@
-# config.py
-
 import streamlit as st
 
 def get_api_details():
-    try:
-        api_key = st.secrets["UPSTOX_API_KEY"]
-        api_secret = st.secrets["UPSTOX_API_SECRET"]
-        redirect_uri = st.secrets["REDIRECT_URI"]
-        return api_key, api_secret, redirect_uri
-    except:
-        return None, None, None
+    return (
+        st.secrets["UPSTOX_API_KEY"],
+        st.secrets["UPSTOX_API_SECRET"],
+        st.secrets["REDIRECT_URI"]
+    )
 
 def get_access_token():
-    return st.session_state.get("access_token", None)
+    return st.session_state.get("access_token")
 
 API_KEY, API_SECRET, REDIRECT_URI = get_api_details()
 ACCESS_TOKEN = get_access_token()
